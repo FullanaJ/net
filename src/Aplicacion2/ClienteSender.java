42272b1219c77ea6580d4b53b78695a520883e7c
Jorge Fullana Estela <jorgefullana3@gmail.com>
2023-12-01 00:56:11 +0100

    s

--- src/Aplicacion2/ClienteSender.java
+++ src/Aplicacion2/ClienteSender.java
@@ -2,15 +2,48 @@ package Aplicacion2;
 
 import Aplicacion1.ConexionCli;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.Base64;
 
-public class ClienteSender extends ConexionCli {
-    public ClienteSender(String tipo) throws IOException {
-        super(tipo);
-    }
+public class ClienteSender  {
 
-    @Override
     public void startClient() {
+        try {
+            DatagramSocket socketUDP = new DatagramSocket();
+            BufferedReader reader = new BufferedReader(new java.io.InputStreamReader(System.in));
+
+            System.out.print(">> ");
+            String input = reader.readLine();
+            String encodedString = Base64.getEncoder().encodeToString(input.getBytes());
+            input = "#" + encodedString + "#";
+            byte[] encoded = input.getBytes();
+
+            InetAddress serverHost = InetAddress.getByName("localhost");
+            int serverPort = 6789;
+
+            DatagramPacket sendDatagram = new DatagramPacket(encoded, encoded.length, serverHost, serverPort);
+
+            socketUDP.send(sendDatagram);
+
+            byte[] buffer = new byte[1000];
+            DatagramPacket answerDatagram = new DatagramPacket(buffer, buffer.length);
+            socketUDP.receive(answerDatagram);
+
+            String received = new String(answerDatagram.getData());
+            System.out.print("Respuesta: ");
+            System.out.println(received);
+
+            socketUDP.close();
+        } catch (SocketException e) {
+            System.out.println("Socket: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO: " + e.getMessage());
+        }
 
     }
 
--- src/Aplicacion2/ServidorFile.java
+++ src/Aplicacion2/ServidorFile.java
@@ -3,6 +3,10 @@ package Aplicacion2;
 import Aplicacion1.ConexionSer;
 
 import java.io.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.Base64;
 
 public class ServidorFile extends ConexionSer {
     public ServidorFile(String tipo) throws IOException {
@@ -11,14 +15,29 @@ public class ServidorFile extends ConexionSer {
 
     @Override
     public void startServer() {
+
         try {
-            cs = ss.accept();
-            InputStream is = cs.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader bf = new BufferedReader(isr);
-            String linea = bf.readLine();
+            DatagramSocket socketUDP = new DatagramSocket(6789);
+            byte[] buffer = new byte[1000];
+
+            while (true) {
+                DatagramPacket receivedDatagram = new DatagramPacket(buffer, buffer.length);
+                socketUDP.receive(receivedDatagram);
+
+                System.out.print("Datagrama recibido del host: " + receivedDatagram.getAddress());
+                System.out.println(" desde el puerto remoto: " + receivedDatagram.getPort());
+                String data = new String(receivedDatagram.getData(), "US-ASCII");
+                data = data.replaceAll("#", "");
+                data = new String(Base64.getMimeDecoder().decode(data.getBytes()));
+
+                DatagramPacket responseDatagram = new DatagramPacket(data.getBytes(), data.getBytes().length, receivedDatagram.getAddress(), receivedDatagram.getPort());
+
+                socketUDP.send(responseDatagram);
+            }
+        } catch (SocketException e) {
+            System.out.println("Socket: " + e.getMessage());
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("IO: " + e.getMessage());
         }
     }
 }
